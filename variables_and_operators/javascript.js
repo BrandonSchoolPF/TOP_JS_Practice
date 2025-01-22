6adebd6d6324e5bb57099b6ec3d1dfbe1e828e7e
Brandon Ly <89619512+BrandonSchoolPF@users.noreply.github.com>
2025-01-21 23:13:42 -0500

    completed Function Basics of TOP

--- variables_and_operators/javascript.js
+++ variables_and_operators/javascript.js
@@ -1,5 +1,14 @@
-let name = "John";
-let surname = "Doe";
+let a = 10;
 
-console.log(name);
-console.log(surname);
+console.log(a);
+console.log(a * 9);
+
+let b = 7 * a;
+
+console.log(b)
+
+let max = 57;
+
+let actual = max - 13;
+
+let percentage = actual / max;
